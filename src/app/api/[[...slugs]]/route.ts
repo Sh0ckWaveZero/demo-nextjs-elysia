6db6977728f608d4dad6febda55b6b779009d969
Aphidet-Hongseetong <ottooffple@icloud.com>
2024-08-30 13:01:32 +0700

    chore: Update npm dependencies to latest versions

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -2,11 +2,24 @@ import { Elysia, t } from 'elysia'
 
 const app = new Elysia({ prefix: '/api' })
     .get('/', () => 'hello Next')
-    .post('/', ({ body }) => body, {
+    .post('/', ({ body }) => {
+
+        const { name, isOk } = body
+
+        return {
+            name,
+            isOk
+        };
+    }, {
         body: t.Object({
-            name: t.String()
+            name: t.String(),
+            isOk: t.Boolean()
         })
+    })
+    .onError(({ code }) => {
+        if (code === 'NOT_FOUND')
+            return 'Na is handsome :('
     })
 
 export const GET = app.handle
-export const POST = app.handle 
+export const POST = app.handle
